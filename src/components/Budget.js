ab39b5a02c98f1c05fd5b70204e00cacd7668f8e
ayelentatto <ayelentatto@outlook.com>
2023-02-16 10:50:42 -0500

    step1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,29 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const editBudget = (newBudget) => {
+        dispatch({
+            payload: parseInt(newBudget),
+            type: "SET_BUDGET"
+        })
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £{
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budget}
+                    max="20000"
+                    style={{ marginLeft: '2rem', size: 10 }}
+                    onChange={(event) => editBudget(event.target.value)}>
+                </input>
+
+            }</span>
+
         </div>
     );
 };
